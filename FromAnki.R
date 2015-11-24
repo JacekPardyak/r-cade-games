c040b9df77ddf622f022ff147d82fadd0491228d
unknown <a599131@MC0WWPYC.ww930.my-it-solutions.net>
2015-11-24 18:56:38 +0100

    dictionary improved

---
+++ FromAnki.R
@@ -0,0 +1,5 @@
+Sys.setlocale("LC_ALL", locale = "French")
+
+dictionary <- read.table(file="./Collections/dictionary.txt",header = TRUE ,sep=" ",
+                         encoding = "UTF-8", comment.char = "#")
+
